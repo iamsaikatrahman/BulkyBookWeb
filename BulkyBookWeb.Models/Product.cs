c8b1722b9fb64b275a54269eef34e3e90bec51c3
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-09-09 10:53:52 +0600

    Implement View Model

--- BulkyBookWeb.Models/Product.cs
+++ BulkyBookWeb.Models/Product.cs
@@ -12,12 +12,12 @@ namespace BulkyBook.Models
     {
         public int Id { get; set; }
         [Required]
-        public string Title { get; set; }    
-        public string Description { get; set; }
+        public string? Title { get; set; }    
+        public string? Description { get; set; }
         [Required]
-        public string ISBN { get; set; }
+        public string? ISBN { get; set; }
         [Required]
-        public string Author { get; set; }    
+        public string? Author { get; set; }    
         public double ListPrice { get; set; }
         [Required]
         [Range(1, 10000)]
@@ -28,15 +28,15 @@ namespace BulkyBook.Models
         [Required]
         [Range(1,10000)]
         public double Price100 { get; set; }    
-        public string ImageUrl { get; set; }    
+        public string? ImageUrl { get; set; }    
         [Required]
         public int CategoryId { get; set; }    
         [ForeignKey("CategoryId")]
-        public Category Category { get; set; }    
+        public Category? Category { get; set; }    
         [Required]
         public int CoverTypeId { get; set; }
         [ForeignKey("CoverTypeId")]
-        public CoverType CoverType { get; set; }    
+        public CoverType? CoverType { get; set; }    
 
     }
 }
--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,8 +1,8 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Linq;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -26,33 +26,35 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-                u => new SelectListItem
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
                 {
-                    Text = u.Name,
-                    Value = u.Id.ToString()
-                });
-            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
-                u => new SelectListItem 
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
                 {
-                    Text = u.Name, 
-                    Value = u.Id.ToString() 
-                });
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+
+            };
 
             if (id == null || id == 0)
             {
                 //Create product
-                ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
                 //ViewBag.CoverTypeList = CoverTypeList;
-                return View(product);
+                return View(productVM);
             }
             else
             {
                 //Update Product
             }
-            return View(product);
+            return View(productVM);
         }
 
         [HttpPost]
